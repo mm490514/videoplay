e310531e22e1fd19f861c974f0940a536f88b707
Matheus <mm490514@gmail.com>
2023-04-05 22:02:15 -0300

    Upload de arquivos

--- src/Controller/EditVideoController.php
+++ src/Controller/EditVideoController.php
@@ -30,10 +30,18 @@ class EditVideoController implements Controller
         if ($titulo === false) {
             header('Location: /?sucesso=0');
             return;
-        }
+        }        
 
         $video = new Video($url, $titulo);
         $video->setId($id);
+
+        if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+            move_uploaded_file(
+                $_FILES['image']['tmp_name'],
+                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
+            );
+            $video->setFilePath($_FILES['image']['name']);
+        }
 
         
         $success = $this->videoRepository->update($video);
--- views/video-form.php
+++ views/video-form.php
@@ -1,7 +1,9 @@
 <?php require_once __DIR__ . '/inicio-html.php'; ?>
 <main class="container">
-    <form class="container__formulario" method="post">
+    <form class="container__formulario" enctype="multipart/form-data" method="post">
+        
         <h2 class="formulario__titulo">Envie um vídeo!</h2>
+        
         <div class="formulario__campo">
             <label class="campo__etiqueta" for="url">Link embed</label>
             <input name="url" value="<?= $video?->url; ?>" class="campo__escrita" required placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g" id='url' />
@@ -12,7 +14,13 @@
             <input name="titulo" value="<?= $video?->title; ?>" class="campo__escrita" required placeholder="Neste campo, dê o nome do vídeo" id='titulo' />
         </div>
 
+        <div class="formulario__campo">
+            <label class="campo__etiqueta" for="image">Titulo do vídeo</label>
+            <input name="image" accept="image/*" type="file" class="campo__escrita" id='image' />
+        </div>
+
         <input class="formulario__botao" type="submit" value="Enviar" />
+
     </form>
 </main>
 
